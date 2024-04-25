59ee91951a4f92f48416dad2f5ed32fcab412577
AMALPRO <87407213+AMALPRO@users.noreply.github.com>
2024-04-25 18:33:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.get('/stream', (req, res) => {
     const stream = m3u8stream('https://prod-ent-live-gm.jiocinema.com/hls/live/2100299/hd_akamai_androidmob_avc_eng_ipl_s1_m1250424/master.m3u8');
     stream.pipe(res);
 });
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running...');
