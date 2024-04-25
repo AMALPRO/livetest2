f403235626423c5f9c77516d4665c3a745a88945
AMALPRO <87407213+AMALPRO@users.noreply.github.com>
2024-04-25 18:21:39 +0530

    Update Node.js

--- Node.js
+++ Node.js
@@ -3,7 +3,7 @@ const m3u8stream = require('m3u8stream');
 const app = express();
 
 app.get('/stream', (req, res) => {
-    const stream = m3u8stream('http://your-m3u8-url');
+    const stream = m3u8stream('https://prod-ent-live-gm.jiocinema.com/hls/live/2100299/hd_akamai_androidmob_avc_eng_ipl_s1_m1250424/master.m3u8');
     stream.pipe(res);
 });
 
